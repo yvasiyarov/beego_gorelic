b66fd6ffc70c1ff4979a41d218ebfd57dee8808d
Bill Davis <pabdavis@gmail.com>
2014-09-02 16:34:34 -0400

    Changed to use RunMode vs. AppConfig.String call

--- gorelic.go
+++ gorelic.go
@@ -41,7 +41,7 @@ func InitNewrelicAgent() {
 	agent.HTTPTimer = metrics.NewTimer()
 	agent.CollectHTTPStat = true
 
-	if beego.AppConfig.String("runmode") == "dev" {
+	if beego.RunMode == "dev" {
 		agent.Verbose = true
 	}
 	if verbose, err := beego.AppConfig.Bool("NewrelicVerbose"); err == nil {
@@ -51,10 +51,10 @@ func InitNewrelicAgent() {
 	nameParts := []string{beego.AppConfig.String("appname")}
 	switch strings.ToLower(beego.AppConfig.String("NewrelicAppnameRunmode")) {
 	case "append":
-		nameParts = append(nameParts, beego.AppConfig.String("runmode"))
+		nameParts = append(nameParts, beego.RunMode)
 
 	case "prepend":
-		nameParts = append([]string{beego.AppConfig.String("runmode")}, nameParts...)
+		nameParts = append([]string{beego.RunMode}, nameParts...)
 	}
 	agent.NewrelicName = strings.Join(nameParts, SEPARATOR)
 	agent.Run()
